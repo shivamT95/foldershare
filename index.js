7c4061b47f2f94859c2a78c31325803e5eb35643
Shivam Tyagi <shivam.tyagi.apm13@itbhu.ac.in>
2015-07-14 02:15:09 +0530

    Added requesthandler for viewing files

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 var handle = {}
 handle["/"]=requestHandlers.start;
+handle["/folder"]=requestHandlers.folder;
 server.start(router.route,handle);
--- requestHandlers.js
+++ requestHandlers.js
@@ -1,3 +1,4 @@
+fs = require('fs')
 function start(response) {
   console.log("Request handler 'start' was called.");
     var body = '<html>'+
@@ -6,11 +7,38 @@ function start(response) {
     'content="text/html; charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    'Hello World'
+    '<H1><center> FolderShare </center></H1><br>' +
+    '<H2><a href="/folder"> View folder </a></H2>' +
     '</body>'+
     '</html>';
     response.writeHead(200, {"Content-Type": "text/html"});
   response.write(body);
   response.end();
 }
+function folder(res) {
+  fs.readdir('data',function (err,data){
+    if(err)
+    {
+      console.log(err);
+      res.write("Something went wrong");
+      res.end();
+      return;
+    }
+   var body = '<html>'+
+    '<head>'+
+    '<title> Folder </title>'+
+    '</head>'+
+    '<body>'+
+   '<H3>Folder has following files :-<br></H3>';
+    for(x in data){
+      console.log(data[x]);
+      body+='<b>'+data[x]+'</b><br>';
+    }
+    body+='</body></html>'
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write(body);
+    res.end();
+  });
+}
 exports.start = start;
+exports.folder = folder;
